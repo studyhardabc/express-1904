5ddab84f87b9478590b8bbf56a5024317d51bfdc
studyhardabc <974224401@qq.com>
2020-03-31 22:27:37 +0800

    不x允许重复注册

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,25 +1,38 @@
 const UserModer = require('../models/userModels');
 
+// exports.register = async (req,res) => {
+//     //要获取前端传递过来的用户信息 body方法接收
+//     // const {email,password,nickname} = req.body;
+
+//     //Moder.create()写入数据库
+// //     await UserModer.create(
+// //         Object.assign({}, req.body, {
+// //         password: bcryptjs.hashSync(req.body.password)
+// //     })
+// // );
+
+//     //上面代码可以不修改
+//     await UserModer.create(req.body);
+
+//     //响应
+//     res.send({
+//         code: 0,
+//         msg: '注册成功'
+//     });
+// };
+
 exports.register = async (req,res) => {
-    //要获取前端传递过来的用户信息 body方法接收
-    // const {email,password,nickname} = req.body;
-
-    //Moder.create()写入数据库
-//     await UserModer.create(
-//         Object.assign({}, req.body, {
-//         password: bcryptjs.hashSync(req.body.password)
-//     })
-// );
-
-    //上面代码可以不修改
-    await UserModer.create(req.body);
-
-    //响应
-    res.send({
-        code: 0,
-        msg: '注册成功'
-    });
-};
+    //获取email
+    const {email} = req.body;
+    //判断是否已经注册过，能查找到说明已经注册过
+    const data = await UserModer.findOne({email});
+    if(data){
+        res.send({code: -1,msg: '用户已经注册了'});
+    }else{
+        await UserModer.create(req.body);
+        res.send({code:0, msg: '用户名注册成功'});
+    }
+}
 
 exports.login = (req,res) => {
     res.send('用户登录');
